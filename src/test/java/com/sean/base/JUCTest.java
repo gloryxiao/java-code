4f42c73d2e1dad7adc11b2d97b9a7498c61ae613
sean(xiaojun) <xiaojun1@xiaomi.com>
2020-12-18 17:15:31 +0800

    [update] 添加logger日志

--- src/test/java/com/sean/base/JUCTest.java
+++ src/test/java/com/sean/base/JUCTest.java
@@ -1,5 +1,6 @@
 package com.sean.base;
 
+import lombok.extern.log4j.Log4j2;
 import org.junit.Test;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
@@ -9,13 +10,14 @@ import java.util.concurrent.locks.LockSupport;
  * JUC中封装的LockSupport可以使用来停止/授权 线程起停
  */
 
+@Log4j2
 public class JUCTest {
 
     @Test
     public void testParkUnparkFunc() {
         Thread t = new Thread(() -> {
             for(int i = 0; i < 5; i++) {
-                System.out.println("hahaha test sean: " + i);
+                log.info("[testParkUnparkFunc], echo: {}", i);
                 try {
                     TimeUnit.SECONDS.sleep(1);
                 } catch (Exception e) {
